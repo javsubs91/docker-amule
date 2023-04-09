edfe544638405d046183d1aebb6c85873bf12563
javsubs91 <80193522+javsubs91@users.noreply.github.com>
2023-04-09 21:58:04 +0700

    Update o.py

--- doc/o.py
+++ doc/o.py
@@ -14,7 +14,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-url = "https://ouo.io/JvT2fO"  # @param {type:"string"}
+url = "https://ouo.io/P96o8k"  # @param {type:"string"}
 
 
 def RecaptchaV3(ANCHOR_URL):
