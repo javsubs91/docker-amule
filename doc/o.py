9872d70489bffdc7f499c1cb6e1f88bcfaf94550
javsubs91 <80193522+javsubs91@users.noreply.github.com>
2023-04-09 19:02:47 +0700

    Update o.py

--- doc/o.py
+++ doc/o.py
@@ -14,7 +14,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-url = "https://ouo.io/sBjqpH"  # @param {type:"string"}
+url = "https://ouo.io/42uNhn"  # @param {type:"string"}
 
 # -------------------------------------------
 # RECAPTCHA v3 BYPASS
